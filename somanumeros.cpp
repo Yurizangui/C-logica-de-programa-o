946516c88ae9e0034a4919ebfaafde301caaede4
Yuri zangui <Yuri.zangui@gmail.com>
2026-02-23 11:18:36 +0300

    soma de dois numeros

---
+++ somanumeros.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int num1;
+int num2;
+
+int main(){
+    cout<<"digita dois numeros";
+    cin>> num1>>num2;
+    cout<< num1+num2<<endl;
+    return 0;
+}
